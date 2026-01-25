46dcb24a2192ab51650a337b05651b731a743614
Cheick Tidjane <cheicktidjane312@gmail.com>
2026-01-25 21:08:22 +0000

    Ajout redirection next.config

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // 1. Ta configuration d'images (Sanity) - JE N'AI RIEN TOUCHÉ ICI
   images: {
     remotePatterns: [
       {
@@ -7,6 +8,23 @@ const nextConfig = {
         hostname: 'cdn.sanity.io',
       },
     ],
+  },
+
+  // 2. La nouvelle redirection "Anti-Doublon" (Vercel.app -> .com)
+  async redirects() {
+    return [
+      {
+        source: '/:path*', // S'applique à toutes les pages
+        has: [
+          {
+            type: 'host',
+            value: 'fierlah-website.vercel.app', // Si l'utilisateur est sur l'ancien domaine
+          },
+        ],
+        destination: 'https://www.fierlah-agency.com/:path*', // On l'envoie sur le nouveau
+        permanent: true, // Code 301 (Message "Déménagement définitif" pour Google)
+      },
+    ];
   },
 };
 
